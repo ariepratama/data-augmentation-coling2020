dece1d5fa9f4b84f83b0fc79c3f1e79014386d6e
ariepratama <arie.pratama.s@gmail.com>
2022-04-12 22:03:00 +0800

    add maug and saug

--- run_experiment_maug.sh
+++ run_experiment_maug.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=da-coling2020-saug
+#SBATCH --job-name=da-coling2020-maug
 #SBATCH --partition=standard
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4gb
 #SBATCH --nodes=1
-#SBATCH --output=/home/u26/ariesutiono/da-coling2020/logs/run_experiment_saug.log
+#SBATCH --output=/home/u26/ariesutiono/da-coling2020/logs/run_experiment_maug.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ariesutiono@email.arizona.edu
 #SBATCH --account=clu-ling
@@ -16,10 +16,10 @@ conda init bash
 source ~/.bashrc
 conda activate py38
 python main.py \
---data_folder data/S-sent \
+--data_folder data/M-sent \
 --embedding_type bert \
 --pretrained_dir allenai/scibert_scivocab_cased \
---result_filepath logs/S-sent-aug.log \
+--result_filepath logs/M-sent-maug.log \
 --augmentation GR \
 --replaced_non_terminal NP \
 --num_generated_samples 5 \
