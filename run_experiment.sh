aaf86642e63d627717342b94a6d801aa37dd276b
ariepratama <arie.pratama.s@gmail.com>
2022-04-18 07:35:53 +0800

    add logss

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 #SBATCH --partition=standard
 #SBATCH --account=clu-ling
+#SBATCH --mail-user=ariesutiono@email.arizona.edu
+#SBATCH --mail-type=AL
+
+echo "data dir: ${DATA_DIR}"
+echo "log dir: ${LOG_DIR}"
+echo "data size: ${DATA_SIZE}"
+echo "run datetime: ${RUN_DATETIME}"
+echo "repalced non terminal: ${REPLACED_NON_TERMINAL}"
+echo "num generated sentences: ${NUM_GENERATED_SENTENCES}"
+
 
 module load anaconda/2020
 conda init bash
 source ~/.bashrc
 conda activate py38
+echo "running main script..."
+
 python main.py \
 --data_folder ${DATA_DIR}/${DATA_SIZE}-sent \
 --embedding_type bert \
